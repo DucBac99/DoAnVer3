d55fe23efce97f678bf62efa2ac0f7bf4a68fbb8
DucBac99 <ducbac1411.mta@gmail.com>
2023-05-22 09:42:31 +0700

    Add project files.

---
+++ DoAnVer3/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace DoAnVer3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblLightStatus = new System.Windows.Forms.Label();
+            this.btnTurnOff = new System.Windows.Forms.Button();
+            this.btnTurnOn = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblTemperature = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblLightStatus);
+            this.groupBox1.Controls.Add(this.btnTurnOff);
+            this.groupBox1.Controls.Add(this.btnTurnOn);
+            this.groupBox1.Location = new System.Drawing.Point(12, 117);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(278, 184);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // lblLightStatus
+            // 
+            this.lblLightStatus.AutoSize = true;
+            this.lblLightStatus.Location = new System.Drawing.Point(29, 128);
+            this.lblLightStatus.Name = "lblLightStatus";
+            this.lblLightStatus.Size = new System.Drawing.Size(77, 20);
+            this.lblLightStatus.TabIndex = 2;
+            this.lblLightStatus.Text = "Đèn đang:";
+            // 
+            // btnTurnOff
+            // 
+            this.btnTurnOff.Location = new System.Drawing.Point(142, 73);
+            this.btnTurnOff.Name = "btnTurnOff";
+            this.btnTurnOff.Size = new System.Drawing.Size(94, 29);
+            this.btnTurnOff.TabIndex = 1;
+            this.btnTurnOff.Text = "Tắt đèn";
+            this.btnTurnOff.UseVisualStyleBackColor = true;
+            this.btnTurnOff.Click += new System.EventHandler(this.btnTurnOff_Click);
+            // 
+            // btnTurnOn
+            // 
+            this.btnTurnOn.Location = new System.Drawing.Point(19, 73);
+            this.btnTurnOn.Name = "btnTurnOn";
+            this.btnTurnOn.Size = new System.Drawing.Size(94, 29);
+            this.btnTurnOn.TabIndex = 0;
+            this.btnTurnOn.Text = "Bật đèn";
+            this.btnTurnOn.UseVisualStyleBackColor = true;
+            this.btnTurnOn.Click += new System.EventHandler(this.btnTurnOn_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblTemperature);
+            this.groupBox2.Location = new System.Drawing.Point(364, 117);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(278, 184);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // lblTemperature
+            // 
+            this.lblTemperature.AutoSize = true;
+            this.lblTemperature.Location = new System.Drawing.Point(18, 82);
+            this.lblTemperature.Name = "lblTemperature";
+            this.lblTemperature.Size = new System.Drawing.Size(130, 20);
+            this.lblTemperature.TabIndex = 3;
+            this.lblTemperature.Text = "Nhiệt độ đo được:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label lblLightStatus;
+        private Button btnTurnOff;
+        private Button btnTurnOn;
+        private GroupBox groupBox2;
+        private Label lblTemperature;
+    }
+}
---
+++ DoAnVer3/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Windows.Forms;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
+
+namespace DoAnVer3
+{
+    public partial class Form1 : Form
+    {
+        private IMqttClient mqttClient;
+        private bool isLightOn = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            // Khởi tạo MQTT client
+            mqttClient = new MqttFactory().CreateMqttClient();
+
+            //Cấu hình thông tin kết nối
+            var options = new MqttClientOptionsBuilder()
+                .WithTcpServer("a1p3y2wnw8qrak-ats.iot.ap-southeast-2.amazonaws.com", 8883)
+                .Build();
+
+            // Đăng ký sự kiện kết nối thành công
+            mqttClient.Connected += MqttClient_Connected;
+
+            // Đăng ký sự kiện nhận tin nhắn
+            mqttClient.ApplicationMessageReceived += MqttClient_ApplicationMessageReceived;
+
+            // Kết nối đến máy chủ MQTT
+            await mqttClient.ConnectAsync(options);
+        }
+
+        private void MqttClient_Connected(object sender, MqttClientConnectedEventArgs e)
+        {
+            // Khi kết nối thành công, enable các nút để bật/tắt đèn
+            btnTurnOn.Invoke((MethodInvoker)(() => btnTurnOn.Enabled = true));
+            btnTurnOff.Invoke((MethodInvoker)(() => btnTurnOff.Enabled = true));
+
+            // Subscribe vào topic
+            mqttClient.SubscribeAsync("your/topic");
+        }
+        private async void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                // Ngắt kết nối với broker MQTT
+                await mqttClient.DisconnectAsync();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to disconnect: {ex.Message}");
+            }
+        }
+        private async void btnTurnOn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Gửi MQTT message bật đèn
+                var mqttMessage = new MqttApplicationMessageBuilder()
+                    .WithTopic("board")
+                    .WithPayload("1")
+                    .WithExactlyOnceQoS()
+                    .WithRetainFlag()
+                    .Build();
+
+                await mqttClient.PublishAsync(mqttMessage);
+
+                // Cập nhật trạng thái đèn và hiển thị
+                isLightOn = true;
+                lblLightStatus.Text = "Light Status: ON";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to turn on the light: {ex.Message}");
+            }
+        }
+
+        private async void btnTurnOff_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Gửi MQTT message tắt đèn
+                var mqttMessage = new MqttApplicationMessageBuilder()
+                    .WithTopic("board")
+                    .WithPayload("0")
+                    .WithExactlyOnceQoS()
+                    .WithRetainFlag()
+                    .Build();
+
+                await mqttClient.PublishAsync(mqttMessage);
+
+                // Cập nhật trạng thái đèn và hiển thị
+                isLightOn = false;
+                lblLightStatus.Text = "Light Status: OFF";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to turn off the light: {ex.Message}");
+            }
+        }
+
+        private void HandleMessageReceived(MqttApplicationMessageReceivedEventArgs e)
+        {
+            // Xử lý dữ liệu nhận được từ AWS IoT Core
+            var payload = System.Text.Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+            if (e.ApplicationMessage.Topic == "board")
+            {
+                lblTemperature.Invoke((MethodInvoker)(() =>
+                {
+                    lblTemperature.Text = $"Temperature: {payload}°C";
+                }));
+            }
+        }
+
+    }
+}
